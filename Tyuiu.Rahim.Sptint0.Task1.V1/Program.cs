bdd3dd308f3074c678ef333dfc9c8eb38f7d28fd
Ahmadi1 <Ahmadi@DS10BK3>
2026-01-28 23:09:28 -0800

    Add project files.

---
+++ Tyuiu.Rahim.Sptint0.Task1.V1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.Ahmadi1.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,Word");
+            string neme = "Rahimullaj Gafori";
+            int age = 25;
+            Console.WriteLine("Здравствуйте меня зовут" + neme + "мен " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tyuiu.Rahim.Sptint0.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.Rahim.Sprint0.Task2.V1.Lib;
+
+namespace Tyuiu.Rahim.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Rahimullajgafori";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, a", res);
+        }
+    }
+}
